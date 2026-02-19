b1e62231384cfd96c521b23d0c927a5c17abcf5c
LAPTOP-USOAHHI2\yangsusu <yangsusu2jt@qq.com>
2026-02-20 02:21:32 +0800

    fix: 配置base路径解决资源加载问题

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/my-vue-protect/'
+  // 这里必须是你的仓库名，前面要加斜杠
+  base: '/my-vue-protect/' 
 })
